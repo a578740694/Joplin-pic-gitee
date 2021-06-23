338207f4c6c1b3b610c2f581b89903aee23c5754
lisulong <578740694@qq.com>
2021-06-23 10:42:30 +0800

    增加备份功能，保存用户配置文件  增加异常时的处理

--- window.py
+++ window.py
@@ -1,26 +1,58 @@
-import requests
 import base64
-import time
 import json as JSON
-import re;
-import sys
 import logging
-from PyQt5.QtWidgets import QApplication
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import (QWidget, QLabel, QGridLayout, QLineEdit, QPushButton)
+import os
+import re
+import sys
 from concurrent import futures
 from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
+
+import requests
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import (QWidget, QLabel, QGridLayout, QLineEdit, QPushButton)
 
 from logger import Logger
 
 
 class GridLayout(QWidget):
 
+    #日志文件
+    __log = Logger("log.txt")
+
+    #备份目录名称
+    __backup = "backup"
+
+    #配置文件名称
+    __conf = "conf.txt"
+
     def __init__(self):
         super().__init__()
 
+        #初始化界面
         self.initUI()
-        self.log = Logger("log.txt")
+
+        #加载配置文件
+        if os.path.exists(self.__conf) or os.path.isfile(self.__conf):
+            f = open(self.__conf,mode="r")
+            line = f.readline()
+            if(len(line.strip()) != 0):
+                #解码
+                de = line.encode()
+                de = base64.b16decode(de)
+                de = base64.b64decode(de)
+                de = bytes.decode(de,encoding='unicode-escape')
+
+                list = de.split("|")
+                self.joplinPort.setText(list[0])
+                self.joplinToken.setText(list[1])
+                self.giteeToken.setText(list[2])
+                self.giteeOwner.setText(list[3])
+                self.giteePath.setText(list[4])
+                self.giteeRepo.setText(list[5])
+
+                f.close()
 
     def initUI(self):
 
@@ -30,6 +62,7 @@ class GridLayout(QWidget):
         self.move(300, 300)
         #设置窗口的标题
         self.setWindowTitle('Joplin更改gitee图床')
+        self.setWindowFlags(QtCore.Qt.WindowCloseButtonHint|QtCore.Qt.WindowMinimizeButtonHint)
 
         layout = QGridLayout()
         #layout.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignVCenter)
@@ -93,56 +126,109 @@ class GridLayout(QWidget):
             #根据id 查询笔记内容
             joplinUpdate = "http://127.0.0.1:{}/notes/{}?token={}".format(self.joplinPort.text(),id,self.joplinToken.text())
             joplinBody = joplinUpdate + "&fields=body"
-            resp = requests.get(joplinBody)
+            resp = requests.get(joplinBody,timeout=5)
             body = str(resp.json().get("body"))
 
+            gitee = "https://gitee.com/" + self.list[3]
             #只匹配地址不为gitee的图片链接
-            data = re.findall(r'!\[(.*)\]\(((?!https://gitee.com).*)\)',body)
+            data = re.findall(r'!\[(.*)\]\(((?!' + gitee + ').*)\)',body)
             if(len(data) <= 0):
                 return (0,title)
 
+            #备份笔记 防止修改出错
+            date = datetime.now().strftime('%Y%m%d%H%M%S%f')[:-3]
+            f = open(self.__backup + os.sep +title + "_" + date + ".md","w+")
+            f.write(body)
+            f.close();
+
+            files = []
             for v in data:
-                #使用正则表达式查询 笔记内容中的 图片链接
-                addr = v[1];
+                try:
+                    #使用正则表达式查询 笔记内容中的 图片链接
+                    addr = v[1];
 
-                #获取网络图片并转为base64
-                base = base64.b64encode(requests.get(addr).content)
-                time_tup = str(round(time.time() * 1000))
+                    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36"}
 
-                path = self.giteePath.text() + "/" + time_tup + ".png"
+                    #获取网络图片并转为base64
+                    picData = requests.get(addr,timeout=20,headers=headers)
+                    if(picData.status_code != 200):
+                        self.__log.logger.error(picData.content)
+                        raise Exception("图片下载失败")
 
-                url = "https://gitee.com/api/v5/repos/{}/{}/contents/{}".format(self.giteeOwner.text(),self.giteeRepo.text(),path)
-                params = {"access_token": self.giteeToken.text(), "owner": self.giteeOwner.text(), "repo": self.giteeRepo.text(), "path": path, "content": base, "message": "上传图片"}
+                    base = base64.b64encode(picData.content)
+                    time_tup = datetime.now().strftime('%Y%m%d%H%M%S%f')[:-3]
 
-                #上传图片到Gitee 并返回图片地址
-                resp = requests.post(url,data=params)
-                pic = resp.json().get("content").get("download_url")
+                    path = self.list[4] + "/" + time_tup + ".png"
+                    files.append(path)
 
-                #把笔记中的图片链接替换为Gitee的图片链接
-                body = body.replace(addr, pic)
+                    url = "https://gitee.com/api/v5/repos/{}/{}/contents/{}"
+                    postUrl = url.format(self.list[3],self.list[5],path)
+                    params = {"access_token": self.list[2], "owner": self.list[3], "repo": self.list[5], "path": path, "content": base, "message": "上传图片"}
+
+                    #上传图片到Gitee 并返回图片地址
+                    resp = requests.post(postUrl,data=params,timeout=20)
+                    pic = resp.json().get("content").get("download_url")
+                except Exception as e:
+                    if len(files) != 0:
+                        #如果请求失败 或者 获取不到数据 删除之前上传的文件
+                        getUrl = url.format(self.list[3],self.list[5],self.list[4]) + "?access_token=" + self.list[2]
+                        sha = requests.get(getUrl,timeout=20).json().get("sha")
+
+                        params["sha"] = sha
+                        for file in files:
+                            params["path"] = file
+                            requests.delete(postUrl,data=params,timeout=20)
+
+                    raise Exception(e)
+                else:
+                    #把笔记中的图片链接替换为Gitee的图片链接
+                    body = body.replace(addr, pic)
+
 
             params = {"body": body}
             #更新笔记
-            resp = requests.put(joplinUpdate,data=JSON.dumps(params))
+            resp = requests.put(joplinUpdate,data=JSON.dumps(params),timeout=5)
 
             return (1,title)
         except Exception as e:
             logging.exception(e)
-            self.log.logger.error(e)
+            self.__log.logger.error(e)
             return (-1,title)
 
     def buttonClick(self):
-        if(len(self.joplinToken.text().strip()) == 0 or len(self.joplinPort.text().strip()) == 0 or
-                len(self.giteeToken.text().strip()) == 0 or len(self.giteeRepo.text().strip()) == 0 or
-                len(self.giteePath.text().strip()) == 0 or len(self.giteeOwner.text().strip()) == 0):
+        joplinPort = self.joplinPort.text().strip();
+        joplinToken = self.joplinToken.text().strip();
+        giteeToken = self.giteeToken.text().strip();
+        giteeRepo = self.giteeRepo.text().strip();
+        giteePath = self.giteePath.text().strip();
+        giteeOwner = self.giteeOwner.text().strip();
+
+        self.list = [joplinPort,joplinToken,giteeToken,giteeOwner,giteePath,giteeRepo]
+        if(len(self.list[5]) == 0 or len(self.list[0]) == 0 or
+                len(self.list[1]) == 0 or len(self.list[2]) == 0 or
+                len(self.list[3]) == 0 or len(self.list[4]) == 0):
             QtWidgets.QMessageBox.warning(self, "警告", "数据不能为空", QtWidgets.QMessageBox.Cancel)
             return
 
         b = False
         try:
+            #创建备份目录
+            if not os.path.exists(self.__backup) or not os.path.isdir(self.__backup):
+                os.mkdir(self.__backup)
+
+            #用户数据写入配置文件
+            f = open(self.__conf,mode="w+")
+            en = self.list[0] + "|" + self.list[1] + "|" + self.list[2] + "|" + self.list[3] + "|" + self.list[4] + "|" + self.list[5]
+            en = en.encode(encoding='unicode-escape')
+            en = base64.b64encode(en)
+            en = base64.b16encode(en)
+            en = bytes.decode(en)
+            f.write(en);
+            f.close()
+
             #查询包含 /![] 的笔记
-            joplinSearch = "http://127.0.0.1:{}//search?query=/![]&type=note&token={}".format(self.joplinPort.text(),self.joplinToken.text())
-            resp = requests.get(joplinSearch)
+            joplinSearch = "http://127.0.0.1:{}//search?query=/![]&type=note&token={}".format(self.list[0],self.list[1])
+            resp = requests.get(joplinSearch,timeout=5)
 
             #返回多个笔记的id
             data = resp.json().get("items")
@@ -154,16 +240,16 @@ class GridLayout(QWidget):
                 for done in done_iter:
                     (code,title) = done.result();
                     if(code == 0):
-                        self.log.logger.info("笔记标题: " + title +  " 【忽略】")
+                        self.__log.logger.info("笔记标题: " + title +  " 【忽略】")
                     if(code == 1):
-                        self.log.logger.info("笔记标题: " + title +  " 【完成】")
+                        self.__log.logger.info("笔记标题: " + title +  " 【完成】")
                     if(code == -1):
-                        self.log.logger.error("笔记标题: " + title +  " 【错误】")
+                        self.__log.logger.error("笔记标题: " + title +  " 【错误】")
                         b = True
 
         except Exception as e:
             logging.exception(e)
-            self.log.logger.error(e)
+            self.__log.logger.error(e)
             QtWidgets.QMessageBox.warning(self, "警告", "处理出现异常，请查看日志！！", QtWidgets.QMessageBox.Cancel)
         else:
             if(b):
